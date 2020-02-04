0af5cc3651339f43bf7ecbc9f9af749dfc5874d4
yde <philip.ydehed@gmail.com>
2020-02-04 16:56:06 +0100

    getting account_id when clicking on dropdown option

--- public/app/scripts/script.js
+++ public/app/scripts/script.js
@@ -16,12 +16,18 @@ $("button").on("click", function (event) {
 $.ajax({
     url: 'http://localhost:8888/banking/bank-transfers/public/api/index.php',
     success: function (d) {
-
+        
         for (var i = 0; i < d.data.length; i++) {
             var obj = d.data[i];
 
-            $("select").append(`<option>  ${obj.firstName}  ${obj.lastName} ${obj.balance}kr</option>`);
-
+            $("select").append(`<option class="option">${obj.account_id}  ${obj.firstName}  ${obj.lastName} ${obj.balance}kr</option>`);
+           
         }
     }
+});
+
+$("select").change("click", function (e) {
+    var id = this.value[0];
+    console.log(id);
+    
 });
